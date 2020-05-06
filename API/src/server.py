adc1417e8e7b4313de42a92187b7452070934ade
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2020-05-06 17:31:40 +0100

    Integrated yfinance module

--- API/src/server.py
+++ API/src/server.py
@@ -1,10 +1,21 @@
 from flask import Flask
-import json
+from flask import jsonify
+import yfinance as yf
 
 app = Flask(__name__)
 
 ### Session-time variables ###
-stocks = []
+stocks = dict()
+
+def buildStockList():
+    """ Building the API data using yfinance """
+
+    data = yf.download("AAPL SPG MSFT", start="2009-01-01", group_by="ticker")
+
+    companies = ['MSFT', 'AAPL', 'SPG']
+    
+    for company in companies:
+        stocks.update( {company : data[company]} )
 
 ### HTTP Request Methods ###
 @app.route('/')
@@ -12,10 +23,13 @@ def simpleRequest():
     """ Testing if connection can be made to API """
     return "API listening ..."
 
-@app.route('/stocks')
+@app.route('/stocks', methods=['GET'])
 def getStocks():
     """ Getting session-time stock data """
-    return json.dumps(stocks)
+    buildStockList()
+    for item in stocks.items():
+        print(item)
+    return "Output printed"
 
 if __name__ == '__main__':
     app.run()
