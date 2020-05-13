325e302bf3d6f5dd5ab343beef95e551ee9b7b00
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2020-05-13 14:28:36 +0100

    Further implemented Firebase functionality. Users can download data from the Firebase Database (ie their portofolio). Fixes.

--- Client/dividend-analyser/dividend-analyser/Usersys/Transactions/TransactionClient.swift
+++ Client/dividend-analyser/dividend-analyser/Usersys/Transactions/TransactionClient.swift
@@ -18,6 +18,7 @@ public class FirebaseClient {
         self.User = user
     }
     
+    /* Uploads a certain transaction to the Firebase Databse of the current user */
     public func uploadTransactionToUser(transaction: [String], completion: @escaping (Bool) -> (Void)) {
         
         let dbRef = Database.database().reference() // holds the reference to the Realtime Database
@@ -31,6 +32,36 @@ public class FirebaseClient {
         print("Set values in Database.")
         
         completion(true)
+        
+    }
+    
+    /* Downloads the list of transactions (ie: the portofolio) of a user */
+    public func downloadUserTransactions(completion: @escaping ([String]) -> (Void)) {
+        
+        let dbRef = Database.database().reference()
+        
+        let usersRef = dbRef.child("users")
+        
+        let specificUserRef = usersRef.child("\(String(describing: self.User!.uid))")
+        
+        specificUserRef.observeSingleEvent(of: .value, with: { // Transactions download once when user first logins
+            (snapshot) in
+            
+            if let transactions = snapshot.value as? [String] {
+                print("Got data from download : \(transactions)")
+                
+                completion(transactions)
+            }
+            else {
+                completion([])
+            }
+        }) { (error) in
+            if error == nil { // Something occured while downloading the data
+                print("error = \(error)")
+                
+                completion([])
+            }
+        }
         
     }
     
